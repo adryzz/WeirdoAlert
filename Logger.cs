64b1b41ccc90205bc3e6c540ed9541ef1f004455
Adryzz <46694241+adryzz@users.noreply.github.com>
2020-07-25 10:23:21 +0200

    Add files via upload

---
+++ Logger.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+    public class Logger : IDisposable
+    {
+        public string LogName;
+        StreamWriter Stream;
+        public Logger(string logname)
+        {
+            LogName = logname;
+            Stream = File.AppendText(LogName);
+            Stream.AutoFlush = true;
+        }
+
+        public Logger()
+        {
+            DateTime now = DateTime.Now;
+            LogName = String.Format("{0}-{1}-{2}--{3}-{4}-{5}.log", now.Day, now.Month, now.Year, now.Hour, now.Minute, now.Second);
+            Stream = File.AppendText(LogName);
+            Stream.AutoFlush = true;
+        }
+
+        public void Log(string data)
+        {
+            DateTime now = DateTime.Now;
+            data = String.Format("{0}/{1}/{2} {3}:{4}:{5}:{6} - {7}", now.Day, now.Month, now.Year, now.Hour, now.Minute, now.Second, now.Millisecond, data);
+            Stream.WriteLine(data);
+        }
+
+        public async Task LogAsync(string data)
+        {
+            DateTime now = DateTime.Now;
+            data = String.Format("{0}/{1}/{2} {3}:{4}:{5}:{6} - {7}", now.Day, now.Month, now.Year, now.Hour, now.Minute, now.Second, now.Millisecond, data);
+            await Stream.WriteLineAsync(data);
+        }
+
+        public void Dispose()
+        {
+            Stream.Flush();
+            Stream.Close();
+            Stream.Dispose();
+        }
+    }
---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace WeirdoAlert
+{
+    class Program
+    {
+        public static Logger MainLogger = new Logger();
+        static void Main(string[] args)
+        {
+            MainLogger.Log("Application Started!");
+            Console.WriteLine("Application Started!");
+            throw new NotImplementedException();
+        }
+    }
+}
